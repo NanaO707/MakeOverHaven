1b6c177444b9a5066134f899c3694d078dfb44be
Stephanie M <shelovesgames965@gmail.com>
2026-03-26 13:59:35 -0400

    trying to categorize clothing item

--- Assets/Scripts/ClothingItem.cs
+++ Assets/Scripts/ClothingItem.cs
@@ -6,9 +6,13 @@ public class ClothingItem : ScriptableObject
     public string ClothingName;
 
     //cool is alt
-    public enum Style { Cute, Cool, Athletic, Goth, Academia, Elegant};
-    public enum Type { Top, Bottom, Headwear, Footwear};
-    
+    //public enum Style { Cute, Cool, Athletic, Goth, Academia, Elegant};
+    public enum Type { Top, Bottom, Headwear, Footwear };
+    public enum Style { Sweater, Shirt, Blouse, Skirt, Pants, Dress};
+    public enum Color { Pink, Brown, Blue, Black, White, Purple};
+
+    public int stylePts;
+
     public int Price;
 
     public Sprite Icon;
