4eca80286b617327aebaaf1ae5a31c8880b4ba30
Stephanie M <shelovesgames965@gmail.com>
2026-04-18 19:05:25 -0400

    adding new girl models with scriptable objects

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -3,17 +3,14 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Client : ScriptableObject
 {
-    public string[] StylePreference;
 
-    //cool is alt
-    //public enum Style { Cute, Cool, Athletic, Goth, Academia, Elegant };
-    
     public int StylePointsGoal;
 
     public int Budget;
 
     public Sprite Icon;
 
+    public string Name = "...";
     public string BadReview = "...";
     public string GoodReview = "...";
 }
